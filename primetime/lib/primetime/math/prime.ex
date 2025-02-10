8d16bc0c99e2f1b685d38ca22e41616e86ef9cf0
Samuel Owen <SamHirael@gmail.com>
2025-02-10 22:25:49 +0000

    test: add more functionality and tests for is_prime

--- primetime/lib/primetime/math/prime.ex
+++ primetime/lib/primetime/math/prime.ex
@@ -1,6 +1,20 @@
 defmodule Primetime.Math.Prime do
   @moduledoc false
-  def is_prime(number) when is_number(number) do
+
+  def is_prime(1) do
+    # One is a unit, not a number. Look it up on Wikipedia.
     false
+  end
+
+  def is_prime(2) do
+    true
+  end
+
+  def is_prime(number) do
+    # Filter all even numbers
+    case Integer.mod(number, 2) do
+      0 -> false
+      _ -> true
+    end
   end
 end
--- primetime/test/math/prime_test.exs
+++ primetime/test/math/prime_test.exs
@@ -3,7 +3,19 @@ defmodule Primetime.Math.PrimeTest do
 
   import Primetime.Math.Prime
   # No DB hits or anything like that, these are pure unit tests so can be async
-  test "is_prime returns false for even numbers" do
-    assert is_prime(2) == false
+  test "is_prime returns false for even numbers that aren't 2" do
+    assert is_prime(2) === true
+  end
+
+  test "is_prime returns false for 1" do
+    assert is_prime(1) === false
+  end
+
+  test "is_prime returns false for 100" do
+    assert is_prime(100) === false
+  end
+
+  test "is_prime returns true for 3" do
+    assert is_prime(100) === false
   end
 end
