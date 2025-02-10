c750202e6a47e76689c4ff57580cfbe7dc74f9a5
Samuel Owen <SamHirael@gmail.com>
2025-02-10 22:09:31 +0000

    test: pass first prime test

---
+++ primetime/lib/primetime/math/prime.ex
@@ -0,0 +1,6 @@
+defmodule Primetime.Math.Prime do
+  @moduledoc false
+  def is_prime(number) when is_number(number) do
+    false
+  end
+end
--- primetime/test/math/prime_test.exs
+++ primetime/test/math/prime_test.exs
@@ -1,7 +1,7 @@
 defmodule Primetime.Math.PrimeTest do
   use ExUnit.Case, async: true
 
-  import Primetime.Math
+  import Primetime.Math.Prime
   # No DB hits or anything like that, these are pure unit tests so can be async
   test "is_prime returns false for even numbers" do
     assert is_prime(2) == false
