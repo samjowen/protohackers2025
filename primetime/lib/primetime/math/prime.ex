530ed78de7301af42d51f8fbd86495a359986879
Samuel Owen <SamHirael@gmail.com>
2025-02-10 22:36:54 +0000

    feat: add float support for is_prime?/1

--- primetime/lib/primetime/math/prime.ex
+++ primetime/lib/primetime/math/prime.ex
@@ -1,20 +1,27 @@
 defmodule Primetime.Math.Prime do
   @moduledoc false
 
-  def is_prime(1) do
+  def is_prime?(1) do
     # One is a unit, not a number. Look it up on Wikipedia.
     false
   end
 
-  def is_prime(2) do
+  def is_prime?(2) do
     true
   end
 
-  def is_prime(number) do
+  def is_prime?(number) when is_integer(number) do
     # Filter all even numbers
     case Integer.mod(number, 2) do
       0 -> false
       _ -> true
     end
+  end
+
+  def is_prime?(number) when is_float(number) do
+    # No, decimal numbers are not considered prime because prime numbers
+    # are defined for natural numbers greater than 1 with exactly two distinct
+    # positive divisors, and decimals do not meet this criterion.
+    false
   end
 end
--- primetime/test/math/prime_test.exs
+++ primetime/test/math/prime_test.exs
@@ -3,19 +3,23 @@ defmodule Primetime.Math.PrimeTest do
 
   import Primetime.Math.Prime
   # No DB hits or anything like that, these are pure unit tests so can be async
-  test "is_prime returns false for even numbers that aren't 2" do
-    assert is_prime(2) === true
+  test "is_prime? returns false for even numbers that aren't 2" do
+    assert is_prime?(2) === true
   end
 
-  test "is_prime returns false for 1" do
-    assert is_prime(1) === false
+  test "is_prime? returns false for 1" do
+    assert is_prime?(1) === false
   end
 
-  test "is_prime returns false for 100" do
-    assert is_prime(100) === false
+  test "is_prime? returns false for 100" do
+    assert is_prime?(100) === false
   end
 
-  test "is_prime returns true for 3" do
-    assert is_prime(100) === false
+  test "is_prime? returns true for 3" do
+    assert is_prime?(100) === false
+  end
+
+  test "is_prime? returns false for floats" do
+    assert is_prime?(1.00) === false
   end
 end
