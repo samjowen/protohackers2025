e4f68bf1ff6f6b1e15f157bfadf8225fc9a61099
Samuel Owen <SamHirael@gmail.com>
2025-02-21 22:10:19 +0000

    test: receive a malformed request, send back a single malformed response, and disconnect the client

--- primetime/test/integration_test.exs
+++ primetime/test/integration_test.exs
@@ -10,8 +10,7 @@ defmodule Primetime.IntegrationTest do
   @tag timeout: 100
   test "it can recieve a json string that has the method isPrime" do
     {:ok, socket} = :gen_tcp.connect(~c"localhost", 80, mode: :binary, active: false)
-    :gen_tcp.send(socket, ~s({"method":"isPrime","number":123}
-))
+    :gen_tcp.send(socket, ~s({"method":"isPrime","number":123}))
 
     case :gen_tcp.recv(socket, 0) do
       {:ok, packet} ->
@@ -21,5 +20,24 @@ defmodule Primetime.IntegrationTest do
         Logger.debug(":error in recv: #{IO.inspect(reason)}")
         :gen_tcp.close(socket)
     end
+  end
+
+  @tag timeout: 100
+  test "receive a malformed request, send back a single malformed response, and disconnect the client" do
+    {:ok, socket} = :gen_tcp.connect(~c"localhost", 80, mode: :binary, active: false)
+    # Malformed json terminated by newline
+    :gen_tcp.send(socket, ~s({"method":"isPrime","number"::123}#{<<10>>}))
+
+    case :gen_tcp.recv(socket, 0) do
+      {:ok, packet} ->
+        # Asserting that the pakcket we get back is indeed malformed
+        ^packet = ~s({"method":"isPrime","number"::123})
+
+      _ ->
+        nil
+    end
+
+    # Socket should already be closed and return {:error, reason}
+    {:error, :closed} = :gen_tcp.close(socket)
   end
 end
